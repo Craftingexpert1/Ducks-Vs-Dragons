2b60bf98c2da16e56058d139350b6803533b7ae3
Craftingexpert1 <dev.velp@gmail.com>
2019-01-15 18:46:16 -0500

    d

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var stop = false;
 var player;
 var flip = false;
 var platforms = [];
-var rckbtm = cnvs.height - 36;var map=[];var ai_grav=0;
+var rckbtm = cnvs.height - 49;var map=[];var ai_grav=0;var player_health=10;var ai_health=10;
 function sprite(img, x, y, width, height, deg, flip, flop, center) {
 	this.img = img;
 	this.x = x;
@@ -45,52 +45,70 @@ function sprite(img, x, y, width, height, deg, flip, flop, center) {
 	this.color=color;this.width=width;this.height=height;this.x=x;this.y=y;
     foo.fillStyle = color;
     foo.fillRect(x,y,width,height);
-} function coc(o1, o2) {
+} function platcrash(rect1, rect2) {
 	var crash;		
-	if (o1.y<=o2.y+o2.height/2&&o1.y+o1.height/2>=o2.y+o2.height*3&&o1.x<=o2.x+o2.width&&o1.x+o1.width/2>=o2.x+o1.width/2) {crash=true;} else {crash=false;};
+	if (rect1.x< rect2.x+rect2.width &&
+   rect1.x> rect2.x &&
+   rect1.y+rect1.width/2 < rect2.y + rect2.height &&
+   rect1.y + rect1.height/2 > rect2.y) {
+    crash=true;
+} else {crash=false;};
 	return crash;
-} function grav(h) {
-	setInter
+}  function playcrash(rect1, rect2) {
+	var crash;
+	if(rect1.x<rect2.x+rect1.width/2&&
+   rect1.x>rect2.x &&
+   rect1.y+rect1.width/2 < rect2.y + rect2.height &&
+   rect1.y + rect1.height/2 > rect2.y) {
+		crash="right";
+	} else if (rect1.x==70) {
+		console.log("hey")
+	} else {crash=false;}
+	return crash;
 }
 function draw() {
 	foo.clearRect(0, 0, cnvs.width, cnvs.height)
 	foo.imageSmoothingEnabled = false;
-	player = new sprite(document.querySelector("#duck"), player_x, player_y, 98, 98, 0, flip, false, true);
-	ai = new sprite(document.querySelector("#duck"), ai_x, ai_y, 98, 98, 0, true, false, true);
+	player = new sprite(document.querySelector("#duck"), player_x, player_y, 81, 81, 0, flip, false, true);
+	ai = new sprite(document.querySelector("#duck"), ai_x, ai_y, 81, 81, 0, true, false, true);
 	platforms = [new rect(100, 5, "black", 100, 300), new rect(100, 5, "black", 400, 300), new rect(400, 5, "black", 100, 450)];
 	ai_y+=ai_grav;
-	ai_grav+=0.004;
+	ai_grav+=0.003;
 	player_y += grav;
-	grav +=  0.004;
+	grav+=0.003;
 	if (ai_y > rckbtm) {
 		ai_y = rckbtm;
 		ai_grav=0;
-	}
+	} 
 	if (player_y > rckbtm) {
 		player_y = rckbtm;
 		grav=0;
-	} else if (coc(player, platforms[0])==true || coc(player, platforms[1]) == true|| coc(player, platforms[2])==true|| coc(player, ai)==true) {
+	} else if (platcrash(player, platforms[0])==true || platcrash(player, platforms[1]) == true|| platcrash(player, platforms[2])==true) {
 		grav=0;
+	} if (playcrash(player, ai)=="right") {
+		ai_x-=25;console.log("aaaahhhd");
+	} if (platcrash(ai, platforms[0])==true || platcrash(ai, platforms[1]) == true|| platcrash(ai, platforms[2])==true) {
+		ai_grav=0;
 	}
 	document.onkeydown=(e)=>{
 		e = e || event;
-		map[e.key] = e.type = true;
-		if (map["w"]&&map["a"]||map["ArrowUp"]&&map["ArrowLeft"]) {
-			player_x-=2;flip=true;grav-=0.1;player_y-=1;
-		} else if (map["w"]&&map["d"]||map["ArrowUp"]&&map["ArrowRight"]) {
-			player_x+=2;flip=false;grav-=0.1;player_y-=1;
-		} else if (map["s"]&&map["d"]||map["ArrowDown"]&&map["ArrowRight"]) {
-			player_y+=1;player_x+=2;flip=false;
-		} else if (map["s"]&&map["a"]||map["ArrowDown"]&&map["ArrowLeft"]) {
-			player_y+=1;player_x-=2;flip=true;
-		} else if (map["w"]||map["ArrowUp"]) {
+		map[e.key.toLowerCase()] = e.type = true;
+		if (map["w"]&&map["a"]||map["arrowup"]&&map["arrowleft"]) {
+			player_x-=3;flip=true;grav-=0.1;player_y-=1;
+		} else if (map["w"]&&map["d"]||map["arrowup"]&&map["arrowright"]) {
+			player_x+=3;flip=false;grav-=0.1;player_y-=1;
+		} else if (map["s"]&&map["d"]||map["arrowdown"]&&map["arrowright"]) {
+			player_y+=1;player_x+=3;flip=false;
+		} else if (map["s"]&&map["a"]||map["arrowdown"]&&map["arrowleft"]) {
+			player_y+=1;player_x-=3;flip=true;
+		} else if (map["w"]||map["arrowup"]) {
 			grav -= 0.1;player_y-=1;
-		} else if (map["s"]||map["ArrowDown"]) {
+		} else if (map["s"]||map["arrowdown"]) {
 			player_y+=1;
-		} else if (map["d"]||map["ArrowRight"]) {
-			player_x+=2;flip=false;
-		} else if (map["a"]||map["ArrowLeft"]) {
-			player_x-=2;flip=true;
+		} else if (map["d"]||map["arrowright"]) {
+			player_x+=3;flip=false;
+		} else if (map["a"]||map["arrowleft"]) {
+			player_x-=3;flip=true;
 		}
 	}; document.onkeyup=(e)=>{map=[];};
 }
